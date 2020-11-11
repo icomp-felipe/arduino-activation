151068bed4153775fe5ea6c2d12da3967f9f8a16
Felipe André <fass@icomp.ufam.edu.br>
2020-11-10 23:35:56 -0400

    Created flipflop function

--- activation.ino
+++ activation.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 #include <RTClib.h>
@@ -8,10 +9,9 @@ void     displayTime();
 void     setTime();
 DateTime extractDateTime(char* input);
 
-/*************** Pin Variables ***************/
+/************** Relay Variables **************/
 int R1 = A3;
 int R2 = A2;
-
 
 int RelayNewState, RelayState;
 enum relay { R1Active, R2Active };
@@ -50,7 +50,7 @@ Keypad keypad = Keypad(makeKeymap(keyMap), rowPins, colPins, 4, 4);
 /************** Menu Variables ***************/
 
 // State machine for menu control
-enum menu { menuShowTime, menuSetTime, menuSetActivation };
+enum menu { menuShowTime, menuSetTime, menuFlipFlop };
 int curState;
 
 /*********************************************/
@@ -58,7 +58,6 @@ int curState;
 /*********************************************/
 
 int lastSecond = 0;
-char lastKey = 'A';
 
 void setup() {
 
@@ -95,6 +94,7 @@ void setup() {
 
     }
 
+    // Defining which relay will be first activated
     RelayState = -1;
     RelayNewState = R1Active;
 
@@ -106,22 +106,27 @@ void setup() {
 /******************* LOOP ********************/
 /*********************************************/
 
+DateTime now;
+
 void loop() {
+
+    // Retrieving the current DateTime object
+    now = rtc.now();
 
     // Retrieving keyboard key pressed
     key = keypad.getKey();
 
-    // State machine control
-    if ((key != NO_KEY) && (lastKey != key)) {
+    // State machine control (based on keyboard input)
+    if (key != NO_KEY) {
 
         switch (key) {
 
             case 'A':
-                curState = menuShowTime;
-                break;
-
-            case 'B':
                 curState = menuSetTime;
+                break;
+            
+            case 'B':
+                curState = menuFlipFlop;
                 break;
 
         }
@@ -132,7 +137,8 @@ void loop() {
     switch (curState) {
 
         case menuShowTime:
-            displayTime();
+            if (timeHasPassed())
+                displayTime();
             break;
 
         case menuSetTime:
@@ -140,7 +146,27 @@ void loop() {
             curState = menuShowTime;
             break;
         
+        case menuFlipFlop:
+            flipFlop();
+            curState = menuShowTime;
+            break;
+        
     }
+
+    // Controls the relays behaviour (synced with time)
+    if (timeHasPassed()) {
+        relayControl();
+        lastSecond = now.second();      // Prevents redundant writes to RAM every cicle
+    }
+    
+}
+
+DateTime a = DateTime(0,0,0,7,30,0);
+DateTime b = DateTime(0,0,0,18,0,0);
+
+void relayControl() {
+
+    RelayNewState = (greater(&now,&a) && lower(&now,&b)) ? R1Active : R2Active;
 
     if (RelayState != RelayNewState) {
 
@@ -165,49 +191,30 @@ void loop() {
 
 }
 
-DateTime a = DateTime(0,0,0,7,30,0);
-DateTime b = DateTime(0,0,0,18,0,0);
+// Tells if the time has passed (in terms of seconds)
+boolean timeHasPassed() {
+    return (lastSecond != now.second());
+}
 
 // Displays the current time (every second)
 void displayTime() {
 
-    // Retrieving the current DateTime object
-    DateTime now = rtc.now();
-
-    int curSecond = now.second();
-
-    // If the time has passed (in terms of seconds), the display is updated
-    if (lastSecond != curSecond) {
-
-        sprintf(lcdRow1,"AtivSys|%02d/%02d/%02d", now.day (), now.month (), now.year() - 2000);
-        sprintf(lcdRow2," v.1.0 |%02d:%02d:%02d", now.hour(), now.minute(), curSecond);
+    sprintf(lcdRow1,"AtivSys|%02d/%02d/%02d", now.day (), now.month (), now.year() - 2000);
+    sprintf(lcdRow2," v.1.0 |%02d:%02d:%02d", now.hour(), now.minute(), now.second());
  
-        lcd.clear();
-        lcd.print(lcdRow1);
-        lcd.setCursor(0,1);
-        lcd.print(lcdRow2);
-
-        RelayNewState = (greater(&now,&a) && lower(&now,&b)) ? R1Active : R2Active;
-
-        lastSecond = curSecond;
-
-    }
+    lcd.clear();
+    lcd.print(lcdRow1);
+    lcd.setCursor(0,1);
+    lcd.print(lcdRow2);
 
 }
 
-// Allows the user to manually adjust the system's date and time
-void setTime() {
+char* readKeyboardInput(void (*printer)(char*)) {
 
-    int  index = 0;
+    int index = 0;
 
     // Stores the typed datetime (numbers only: 6 representing date + 6 representing time + 1 storing '\0')
-    char input[13] = "";
-
-    // Displays the menu
-    lcd.clear();
-    lcd.print("Ajustar|__/__/__");
-    lcd.setCursor(0,1);
-    lcd.print(" Data  |__:__:__");
+    char* input = (char*) calloc(13, sizeof(char));
 
     // Wait for user input until the '#' character is typed
     while ((key = keypad.getKey()) != '#') {
@@ -229,15 +236,34 @@ void setTime() {
                     
             }
 
-            // Prints the typed datetime
-            printDate(input);
+            // Prints the typed input using the given printer function
+            printer(input);
             
         }
         
     }
 
+    if (index == 12)
+        return input;
+    
+    free(input);
+
+    return NULL;
+}
+
+// Allows the user to manually adjust the system's date and time
+void setTime() {
+
+    // Displays the menu
+    lcd.clear();
+    lcd.print("Ajustar|__/__/__");
+    lcd.setCursor(0,1);
+    lcd.print(" Data  |__:__:__");
+
+    char* input = readKeyboardInput(printDate);
+    
     // After pressing '*', if the datetime is complete...
-    if (index == 12) {
+    if (input != NULL) {
 
         // ...then it needs to be validated
         DateTime datetime = extractDateTime(input);
@@ -261,6 +287,126 @@ void setTime() {
 
 }
 
+void flipFlop() {
+
+    DateTime mem1, mem2;
+
+    readFlipFlopTimes(&mem1, &mem2);
+
+    if (mem1.isValid() && mem2.isValid()) {
+        sprintf(lcdRow1, " Liga  |%02d:%02d:%02d", mem1.hour(), mem1.minute(), mem1.second());
+        sprintf(lcdRow2, "Desliga|%02d:%02d:%02d", mem2.hour(), mem2.minute(), mem2.second());
+    }
+    else {
+        sprintf(lcdRow1, " Liga  |__:__:__");
+        sprintf(lcdRow2, "Desliga|__:__:__");
+    }
+    
+    // Displays the menu
+    lcd.clear();
+    lcd.print(lcdRow1);
+    lcd.setCursor(0,1);
+    lcd.print(lcdRow2);
+
+    char* input = readKeyboardInput(printTime);
+    
+    // After pressing '*', if the all the times were completely typed...
+    if (input != NULL) {
+
+        // ...then they need to be validated
+        DateTime time1, time2;
+
+        extractTime(input, &time1, &time2);
+        lcd.clear();
+
+        // If the date is valid, then it's copied to the RTC module
+        if (time1.isValid() && time2.isValid()) {
+
+            if (same(&time1,&time2)) {
+                lcd.print("Horarios Iguais!");
+                lcd.setCursor(2,1);
+                lcd.print("Ignorando...");
+            }
+            else {
+
+                if (!same(&mem1, &time1))
+                    saveFlipFlopTime(&time1, 0);
+                
+                if (!same(&mem2, &time2))
+                    saveFlipFlopTime(&time2, 6);
+
+                lcd.print("Configuracao OK!");
+            }
+            
+        }
+        else {
+            lcd.setCursor(3,0);
+            lcd.print("Horario(s)");
+            lcd.setCursor(2,1);
+            lcd.print("Invalido(s)");
+        }
+        
+        delay(3000);
+
+        free(input);
+
+    }
+
+}
+
+void saveFlipFlopTime(DateTime* time, int address) {
+
+    int array[3] = { time->hour(), time->minute(), time->second() };
+
+    EEPROMWriteIntArray(array,address);
+}
+
+void readFlipFlopTimes(DateTime* time1, DateTime* time2) {
+
+    int* array = EEPROMReadIntArray(0);
+    
+    *time1 = DateTime(2020,1,1,array[0],array[1],array[2]);
+    *time2 = DateTime(2020,1,1,array[3],array[4],array[5]);
+
+}
+
+int* EEPROMReadIntArray(int address) {
+
+    int* array = (int*) malloc(6 * sizeof(int));
+
+    for (int i=0; i<6; i++, address++)
+        array[i] = EEPROMReadInt(address++);
+
+    return array;
+}
+
+void EEPROMWriteIntArray(int* array, int address) {
+
+    for (int i=0; i<3; i++, address++)
+        EEPROMWriteInt(address++, array[i]);
+
+}
+
+void EEPROMWriteInt(int p_address, int p_value) {
+
+    byte lowByte  = ((p_value >> 0) & 0xFF);
+    byte highByte = ((p_value >> 8) & 0xFF);
+
+    EEPROM.write(p_address, lowByte);
+    EEPROM.write(p_address + 1, highByte);
+
+}
+
+//This function will read a 2 byte integer from the eeprom at the specified address and address + 1
+unsigned int EEPROMReadInt(int p_address) {
+
+    byte lowByte  = EEPROM.read(p_address);
+    byte highByte = EEPROM.read(p_address + 1);
+
+    return ((lowByte << 0) & 0xFF) + ((highByte << 8) & 0xFF00);
+
+}
+
 // Creates a 'DateTime' object from the given 'input'
 DateTime extractDateTime(char* input) {
 
@@ -276,9 +422,29 @@ DateTime extractDateTime(char* input) {
     return DateTime(year,month,day,hour,min,sec);
 }
 
-void printDate(char* input) {
+void extractTime(char* input, DateTime* time1, DateTime* time2) {
 
-    sprintf(lcdRow1,"__/__/__");
+    // Converting the char input data (from keyboard) to integer values
+    int hour1 = (input[ 0] - '0') * 10 + input[ 1] - '0';
+    int  min1 = (input[ 2] - '0') * 10 + input[ 3] - '0';
+    int  sec1 = (input[ 4] - '0') * 10 + input[ 5] - '0';
+
+    int hour2 = (input[ 6] - '0') * 10 + input[ 7] - '0';
+    int  min2 = (input[ 8] - '0') * 10 + input[ 9] - '0';
+    int  sec2 = (input[10] - '0') * 10 + input[11] - '0';
+
+    *time1 = DateTime(2020,1,1,hour1,min1,sec1);
+    *time2 = DateTime(2020,1,1,hour2,min2,sec2);
+
+}
+
+void printDateTime(char* input, boolean isTimeOnly) {
+
+    if (isTimeOnly)
+        sprintf(lcdRow1,"__:__:__");
+    else
+        sprintf(lcdRow1,"__/__/__");
+
     sprintf(lcdRow2,"__:__:__");
 
     for (int i=0, shift = 0; input[i] != '\0'; i++) {
@@ -313,8 +479,15 @@ void printDate(char* input) {
 
 }
 
+void printDate(char* input) {
+    printDateTime(input,false);
+}
+
+void printTime(char* input) {
+    printDateTime(input,true);
+}
+
 boolean lower(DateTime* a, DateTime* b) {
-	
 	return (a->hour() < b->hour() ||
 				(a->hour() == b->hour() &&
 					(a->minute() < b->minute() ||
@@ -325,12 +498,18 @@ boolean lower(DateTime* a, DateTime* b) {
 }
 
 boolean greater(DateTime* a, DateTime* b) {
-	
 	return (a->hour() > b->hour() ||
 				(a->hour() == b->hour() &&
 					(a->minute() > b->minute() ||
 						(a->minute() == b->minute() && a->second() > b->second())
 					)
 				)
+			);
+}
+
+boolean same(DateTime* a, DateTime* b) {
+	return (a->hour  () == b->hour  () &&
+            a->minute() == b->minute() &&
+            a->second() == b->second()
 			);
 }
